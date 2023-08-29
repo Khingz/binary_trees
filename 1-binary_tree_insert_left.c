0136d379ef0041b88e3e549c4d8bac06742ebb89
Khingz <kingsley995mr@gmail.com>
2023-08-29 02:40:37 -0700

    merge

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,26 +10,26 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (!parent)
-	{
-		return (NULL);
-	}
+        if (!parent)
+        {
+                return (NULL);
+        }
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = value;
-	new->right = NULL;
-	new->parent = parent;
-	if (parent->left == NULL)
-	{
-		parent->left = new;
-		new->left = NULL;
-		return (new);
-	}
-	new->left = parent->left;
-	parent->left = new;
-	return (new);
+        new = malloc(sizeof(binary_tree_t));
+        if (new == NULL)
+        {
+                return (NULL);
+        }
+        new->n = value;
+        new->right = NULL;
+        new->parent = parent;
+        if (parent->left == NULL)
+        {
+                parent->left = new;
+                new->left = NULL;
+                return (new);
+        }
+        new->left = parent->left;
+        parent->left = new;
+        return (new);
 }
